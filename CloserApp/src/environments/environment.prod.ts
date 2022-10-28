26af1db0275620a1382823dd84d9f6eefe32273f
chrisdresba <chrisdresba@gmail.com>
2022-10-28 05:55:04 -0300

    carga nueva clave

--- CloserApp/src/environments/environment.prod.ts
+++ CloserApp/src/environments/environment.prod.ts
@@ -1,11 +1,17 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyD9yXDDKCy18B3Utj4NNnzkfX2yPk_MYj4",
+  /*  apiKey: "AIzaSyD9yXDDKCy18B3Utj4NNnzkfX2yPk_MYj4",
     authDomain: "closer-app-255c5.firebaseapp.com",
     projectId: "closer-app-255c5",
     storageBucket: "closer-app-255c5.appspot.com",
     messagingSenderId: "172843069119",
-    appId: "1:172843069119:web:a175223e3b8b2999c4200a"
+    appId: "1:172843069119:web:a175223e3b8b2999c4200a"*/
+    apiKey: "AIzaSyAeODQL45NzZLmhxfwQir4CD1h2B0jcn4Y",
+    authDomain: "closer-pps.firebaseapp.com",
+    projectId: "closer-pps",
+    storageBucket: "closer-pps.appspot.com",
+    messagingSenderId: "113444583387",
+    appId: "1:113444583387:web:8dcf74f05f2d381dc759fe"
   },
   production: true
 };
