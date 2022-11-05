1187ea7e97275b3b9d3be0ef337b8452476f23a3
AndresWuthrich <andres_wuth@hotmail.com>
2022-11-05 03:11:24 -0300

    lista de espera + pedido (v1)

--- CloserApp/src/app/pages/pedido/pedido.page.ts
+++ CloserApp/src/app/pages/pedido/pedido.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-pedido',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidoPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  async back(){
+    this.router.navigateByUrl('home', { replaceUrl: true });
+  }
+
+  async logout(){
+    await this.authService.logout();
+
+    this.router.navigateByUrl('/', { replaceUrl: true });
+  }
 }
